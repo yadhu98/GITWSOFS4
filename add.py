5cef7f31c6a554779c36c97f50dfa73ade464e57
YADHU KRISHNAN <yadhu98@gmail.com>
2018-04-18 02:02:40 -0400

    Add files via upload

---
+++ add.py
@@ -0,0 +1,12 @@
+def add(i0,i2):
+	sum=i1+i2
+	return sum
+def add3(i1,i2,i3):
+	sum=i1+i2+i3
+	return sum
+def add4(i1,i2,i3,i4):
+	sum=i1+i2+i3+i4
+	return sum
+print add(1,2)
+print add3(1,2,3)
+print add4(1,2,3,4)
